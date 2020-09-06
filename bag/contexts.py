732163b65df98f263b2292519cb4dc26c1a61711
aidant842 <aidant842@gmail.com>
2020-09-06 17:33:55 +0000

    added new logic for adding items to bag, to solve overwriting of item data in the bag when item_id exists already

--- bag/contexts.py
+++ bag/contexts.py
@@ -4,33 +4,23 @@ from products.models import Product
 
 def bag_contents(request):
 
-    bag_items = []
     total = 0
-    delivery = total * .10
-    bag = request.session.get('bag', {})
+    bag = request.session.get('bag', [])
 
-    for item_id in list(bag.keys()):
-        product = get_object_or_404(Product, pk=item_id)
-        quantity = bag[item_id]['item_data']['quantity']
-        size = bag[item_id]['item_data']['size']
-        material = bag[item_id]['item_data']['material']
-        colour = bag[item_id]['item_data']['colour']
+    for item in bag:
+        product = get_object_or_404(Product, pk=item["item_id"])
+        total += product.price * item["quantity"]
+        item["product"] = product
+        item["total"] = product.price * item["quantity"]
 
-        bag_items.append({
-            'product': product,
-            'quantity': quantity,
-            'size': size,
-            'material': material,
-            'colour': colour
-        })
-
+    delivery = int(total * .1)
     grand_total = total + delivery
 
     context = {
-        'bag_items': bag_items,
-        'total': total,
+        'bag_items': bag,
         'delivery': delivery,
         'grand_total': grand_total,
     }
+    print("context", context)
 
     return context
--- bag/views.py
+++ bag/views.py
@@ -12,40 +12,29 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity', 0))
     redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+    bag = request.session.get('bag', [])
     size = request.POST.get('size', None)
     material = request.POST.get('material', None)
     colour = request.POST.get('colour', None)
+    new_item = True
 
-    # Check to see if the product in already in the bag
-    if item_id in list(bag.keys()):
-        print('An item with this ID is already in the bag')
-        # Check to see if the product options are the same
-        # if so, increment quantity
-        if (size == bag[item_id]['item_data']['size'] and
-           material == bag[item_id]['item_data']['material'] and
-           colour == bag[item_id]['item_data']['colour']):
-            print('Product options are the same')
-            bag[item_id]['item_data']['quantity'] += quantity
-        else:
-            print('need logic to not overwrite current'
-                  f'item with this ID: { item_id }')
+    for item in bag:
 
-            bag[item_id] = {'item_data': {'size': size,
-                                          'material': material,
-                                          'colour': colour,
-                                          'quantity': quantity
-                                          }
-                            }
+        if (item["item_id"] == item_id and item["item_size"] == size
+           and item["item_material"] == material
+           and item["item_colour"] == colour):
+            item["quantity"] += quantity
+            new_item = False
+            break
 
-    # if bag is empty, or product with a new ID, add it to the bag
-    else:
-        bag[item_id] = {'item_data': {'size': size,
-                                      'material': material,
-                                      'colour': colour,
-                                      'quantity': quantity
-                                      }
-                        }
+    if new_item:
+        bag.append({
+            "item_id": item_id,
+            "item_size": size,
+            "item_material": material,
+            "item_colour": colour,
+            "quantity": quantity
+        })
 
     print(bag)
     request.session['bag'] = bag
