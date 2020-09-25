9402ecb9021285a6951431c4e728d28325228d59
aidant842 <aidant842@gmail.com>
2020-09-25 10:33:57 +0000

    added checkout form data caching in payment intent

--- static/checkout/js/stripe_elements.js
+++ static/checkout/js/stripe_elements.js
@@ -49,9 +49,47 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('.checkout-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, {
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name=csrfmiddlewaretoken').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+
+    var url = 'cache_checkout_data/';
+
+    $.post(url, postData).done(function(){
+        stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
+            billing_details: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                email : $.trim(form.email.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    state: $.trim(form.county.value),
+                }
+            }
+        },
+        // Post code only in shipping as the post code will come from the card element
+        //for billing
+        shipping: {
+            name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value), 
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
         }
     }).then(function(result) {
         if (result.error) {
@@ -72,4 +110,9 @@ form.addEventListener('submit', function(ev) {
             }
         }
     });
+    }).fail(function(){
+        // Reload to page to show the error message
+       location.reload() 
+    })
+
 });
