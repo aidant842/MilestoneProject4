18b87370fe8eff395aaa10fea586f2e2b9e5b204
aidant842 <aidant842@gmail.com>
2020-10-16 13:53:55 +0000

    corrected spelling error on DEBUG variable

--- PG_Photography/settings.py
+++ PG_Photography/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPEMENT' in os.environ
 
 ALLOWED_HOSTS = ['pg-photography.herokuapp.com', 'localhost']
 
