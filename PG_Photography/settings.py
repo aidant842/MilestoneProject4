e4a357097b3f627e620fa4cb908b6d341a8af095
aidant842 <aidant842@gmail.com>
2020-10-16 14:39:49 +0000

    Changed the debug variable

--- PG_Photography/settings.py
+++ PG_Photography/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPEMENT' in os.environ
+DEBUG = False
+# 'DEVELOPEMENT' in os.environ
 
 ALLOWED_HOSTS = ['pg-photography.herokuapp.com', 'localhost']
 
