20dd93eacd4c36acd59d7ca4e740ad84c20af058
aidant842 <aidant842@gmail.com>
2020-10-16 12:26:09 +0000

    deploying static files to heroku - attempt 2

--- PG_Photography/settings.py
+++ PG_Photography/settings.py
@@ -190,7 +190,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if ' USE_AWS' in os.environ:
-    #Bucket Config
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'pg-photography'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -203,7 +203,7 @@ if ' USE_AWS' in os.environ:
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
-    #Override static and media URLs in production
+    # Override static and media URLs in production
 
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
