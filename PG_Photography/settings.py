86415bab8c3518d47b020ea6f8aaab6f05a30e96
aidant842 <aidant842@gmail.com>
2020-11-04 15:34:49 +0000

    removed contact app due to ongoing db errors; to be rebuilt

--- PG_Photography/settings.py
+++ PG_Photography/settings.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
-    'contact',
 
     # Other
 
--- PG_Photography/urls.py
+++ PG_Photography/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
