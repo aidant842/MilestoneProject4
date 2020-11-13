5bbb4308ca6c34c4a5d8bb276d89745df7f28e86
aidant842 <aidant842@gmail.com>
2020-11-13 15:02:25 +0000

    fixed an issue with the nav that was stopping it behaving as intended

--- static/js/app.js
+++ static/js/app.js
@@ -59,20 +59,26 @@ const navSlide = () => {
         //Burger animation
         burger.classList.toggle('toggle');
     });
+    
     bg.addEventListener('click', () => {
+
+        //Animate Liks
+        if (nav.classList.contains('nav-active')) {
+            
+            navLinksLi.forEach((link, index) => {
+                if(link.style.animation){
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                }
+            });
+
+        }
+
         navLinks.classList.remove('nav-link-active');
         nav.classList.remove('nav-active');
         bg.classList.remove('body-active');
         burger.classList.remove('toggle');
-
-        //Animate Liks
-        navLinksLi.forEach((link, index) => {
-            if(link.style.animation){
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
     });
 
 }
