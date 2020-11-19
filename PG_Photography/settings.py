93945e6cff2c74ced09945cdb6e7d43d1104c002
aidant842 <aidant842@gmail.com>
2020-11-19 17:41:20 +0000

    changed cancel link on edit_product page, pointing to the wrong url

--- PG_Photography/settings.py
+++ PG_Photography/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPEMENT', False)
+if 'DEVELOPEMENT' in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['pg-photography.herokuapp.com', 'localhost']
 
